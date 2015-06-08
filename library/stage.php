7d54e26b41c9bc85a1cf8308478a4ad25d950bb6
Mercedes Egido <meegido@gmail.com>
2015-06-08 18:37:03 +0200

    created custom post type for stage

--- library/stage.php
+++ library/stage.php
@@ -1,5 +1,30 @@
 <?php
 
+function routes_taxonomy() {
+	$labels = array(
+	   'name'              => _x( 'Stage Categories', 'taxonomy general name' ),
+	   'singular_name'     => _x( 'Stage Category', 'taxonomy singular name' ),
+	   'search_items'      => __( 'Search Stage Categories' ),
+	   'all_items'         => __( 'All Stage Categories' ),
+	   'parent_item'       => __( 'Parent Stage Category' ),
+	   'parent_item_colon' => __( 'Parent Stage Category:' ),
+	   'edit_item'         => __( 'Edit Stage Category' ), 
+	   'update_item'       => __( 'Update Stage Category' ),
+	   'add_new_item'      => __( 'Add New Stage Category' ),
+	   'new_item_name'     => __( 'New Stage Category' ),
+	   'menu_name'         => __( 'Stage Categories' ),
+	 );
+
+  $args = array(
+     'labels' => $labels,
+     'hierarchical' => true,
+   );
+  
+  register_taxonomy( 'stage_category', 'stage', $args );
+}
+
+add_action( 'init', 'routes_taxonomy', 0 );
+
 function stage() {
 	$labels = array(
     'name'               => _x( 'Stages', 'post type general name' ),
@@ -22,7 +47,7 @@ function stage() {
     'description'   => 'Stages',
     'public'        => true,
     'menu_position' => 5,
-    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'stage_category' ),
     'has_archive'   => true,
   );
 
