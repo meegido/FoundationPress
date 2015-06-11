ada6db370c845f517617b7ebd311c3abc2d19f7c
Mercedes Egido <meegido@gmail.com>
2015-06-11 11:55:25 +0200

    super commit with grid solution and bigger rows

--- my_content.php
+++ my_content.php
@@ -10,36 +10,36 @@
  */
 
 ?>
-<div class="wide-row">
-<article id="post-<?php the_ID(); ?>"  class="small-2 large-4 columns block-grid full-card" >
-	<header>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="row">
-				<?php the_post_thumbnail( '', array('class' => 'border-img-card') ); ?>
-			</div>
-		<?php endif; ?>
-		<h2 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<span>
-			<?php $tags = get_the_tags(); 
-				foreach ($tags as $tag) {
-					echo '<a href="#">'. $tag->name .'</a>';
-				}
-			 ?>
-		</span>
-	</header>
-	<div class="entry-content">
-		<section class="excerpt-content">
-			<?php the_excerpt( __( 'Continue reading...', 'foundationpress' ) ); ?>
-		</section>
-		<section>
-			<p><?php show_date(get_the_ID()); ?></p>
-			<p><?php show_price(get_the_ID()); ?></p>
-			<br>
-		</section>
-		
-	</div>
-	<footer>
-		
-	</footer>
-</article>
+
+<div class="grid-row">
+	<article id="post-<?php the_ID(); ?>"  class="full-card" >
+		<header>
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="row">
+					<?php the_post_thumbnail( '', array('class' => 'border-img-card') ); ?>
+				</div>
+			<?php endif; ?>
+			<h2 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<span>
+				<?php $tags = get_the_tags(); 
+					foreach ($tags as $tag) {
+						echo '<a href="#">'. $tag->name .'</a>';
+					}
+				 ?>
+			</span>
+		</header>
+		<div class="entry-content">
+			<section class="excerpt-content">
+				<?php the_excerpt( __( 'Continue reading...', 'foundationpress' ) ); ?>
+			</section>
+			<section>
+				<p><?php show_date(get_the_ID()); ?></p>
+				<p><?php show_price(get_the_ID()); ?></p>
+				<br>
+			</section>
+		</div>
+		<footer>
+
+		</footer>
+	</article>
 </div>
