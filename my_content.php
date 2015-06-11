3a37933e85e2f48fa8584a32010d12b5cbbcd71f
Mercedes Egido <meegido@gmail.com>
2015-06-11 12:09:06 +0200

    reordering scss and html

--- my_content.php
+++ my_content.php
@@ -11,7 +11,7 @@
 
 ?>
 
-<div class="grid-row">
+<div class="grid-row small-2 large-4 columns">
 	<article id="post-<?php the_ID(); ?>"  class="full-card" >
 		<header>
 			<?php if ( has_post_thumbnail() ) : ?>
