0dc3569fbc62e0f6fca7e2feda8e01f72e9882f2
Mercedes Egido <meegido@gmail.com>
2015-07-20 17:52:35 +0200

    thumbnail background image single route

--- single-stage.php
+++ single-stage.php
@@ -12,23 +12,34 @@ get_header(); ?>
 <div class="row">
 	<div class="small-12 large-12 columns" role="main">
 
-	<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php do_action( 'foundationpress_before_content' ); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			</header>
-			<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<header>
+					
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php
+						$thumb_id = get_post_thumbnail_id();
+						$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+						$thumb_url = $thumb_url_array[0];
+						?>
+
+					<div class="row">
+						<div class="featured-image" style="background-image: url('<?php echo $thumb_url ?>')" >
+							
+						</div>
+					</div>
+					<?php endif; ?>
+
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+
+				<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+
 			<div class="entry-content">
 
-			<?php if ( has_post_thumbnail() ) : ?>
-				<div class="row">
-					<div class="column">
-						<?php the_post_thumbnail( '', array('class' => 'th') ); ?>
-					</div>
-				</div>
-			<?php endif; ?>
+			
 
 			<?php the_content(); ?>
 			</div>
