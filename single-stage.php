64a386d7851850f73a9ca25cb4de141240b71b8e
elreplicante <replymusic@gmail.com>
2015-07-22 11:42:19 +0200

    narrower single stage content

--- single-stage.php
+++ single-stage.php
@@ -32,35 +32,56 @@ get_header(); ?>
 					</div>
 					<?php endif; ?>
 
-					<h1 class="entry-title"><?php the_title(); ?></h1>
 				</header>
 
-				<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
-
-			<div class="entry-content">
-
-			
-
-			<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-				<span>
-					<?php $tags = get_the_tags();
-					foreach ($tags as $tag) {
-						echo '<a href="#">'. $tag->name .'</a>';
-						}
-					 ?>
-				</span>
-			</footer>
-			<?php do_action( 'foundationpress_post_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_post_after_comments' ); ?>
+				
 		</article>
 	<?php endwhile;?>
 
 	<?php do_action( 'foundationpress_after_content' ); ?>
 
+	</div>
+</div>
+
+<div class="row">
+	<div class="small-12 large-8 columns" role="main">
+		<div class="small-12 large-12 columns" role="main">
+
+			<?php do_action( 'foundationpress_before_content' ); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+					<header>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header>
+
+					<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+
+				<div class="entry-content">
+
+				
+
+				<?php the_content(); ?>
+				</div>
+				<footer>
+					<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+					<span>
+						<?php $tags = get_the_tags();
+						foreach ($tags as $tag) {
+							echo '<a href="#">'. $tag->name .'</a>';
+							}
+						 ?>
+					</span>
+				</footer>
+				<?php do_action( 'foundationpress_post_before_comments' ); ?>
+				<?php comments_template(); ?>
+				<?php do_action( 'foundationpress_post_after_comments' ); ?>
+			</article>
+		<?php endwhile;?>
+
+		<?php do_action( 'foundationpress_after_content' ); ?>
+
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
