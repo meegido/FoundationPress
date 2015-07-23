9a3c6cd5b3aa8844e6eda63ba26908404298e8d3
elreplicante <replymusic@gmail.com>
2015-07-23 20:15:31 +0200

    major changes

--- single-stage.php
+++ single-stage.php
@@ -27,7 +27,7 @@ get_header(); ?>
 
 					<div class="row">
 						<div class="featured-image" style="background-image: url('<?php echo $thumb_url ?>')" >
-
+							<span class="stage-date"><?php echo get_post_custom_values('stage_date')[0];?></span>
 						</div>
 					</div>
 					<?php endif; ?>
@@ -47,36 +47,37 @@ get_header(); ?>
 	<div class="small-12 large-8 columns" role="main">
 		
 
-			<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php do_action( 'foundationpress_before_content' ); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-					<header>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<header>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
 
-					<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+				<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
 
-				<div class="entry-content">
-
-				
-
+			<div class="entry-content">
 				<?php the_content(); ?>
-				</div>
-				<footer>
-					<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-					<span>
-						<?php $tags = get_the_tags();
-						foreach ($tags as $tag) {
-							echo '<a href="#">'. $tag->name .'</a>';
-							}
-						 ?>
-					</span>
-				</footer>
-				<?php do_action( 'foundationpress_post_before_comments' ); ?>
-				<?php comments_template(); ?>
-				<?php do_action( 'foundationpress_post_after_comments' ); ?>
-			</article>
+			</div>
+
+			<div class="map">
+				<iframe src="https://www.google.com/maps/d/embed?mid=zowQ-YO1Y8Lc.kk1rSSWKmEwo" width="100%" height="480"></iframe>
+			</div>
+			<footer>
+				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<span>
+					<?php $tags = get_the_tags();
+					foreach ($tags as $tag) {
+						echo '<a href="#">'. $tag->name .'</a>';
+						}
+					 ?>
+				</span>
+			</footer>
+			<?php do_action( 'foundationpress_post_before_comments' ); ?>
+			<?php comments_template(); ?>
+			<?php do_action( 'foundationpress_post_after_comments' ); ?>
+		</article>
 
 		<?php endwhile;?>
 
@@ -93,17 +94,17 @@ get_header(); ?>
 			  </thead>
 			  <tbody>
 			    <tr>
-			      <td>Content Goes Here</td>
+			      <td><?php echo get_post_custom_values('stage_meeting')[0];?></td>
 			    </tr>
 			  </tbody>
 			  <thead>
 			    <tr>
-			      <th class="<?php echo get_post_custom_values('stage-district')[0];?>">Fin de la ruta</th>
+			      <th class="<?php echo get_post_custom_values('stage-district')[0];?>">Hora</th>
 			    </tr>
 			  </thead>
 			  <tbody>
 			    <tr>
-			      <td>Content Goes Here</td>
+			      <td><?php echo get_post_custom_values('stage_hour')[0];?></td>
 			    </tr>
 			  </tbody>
 			  <thead>
@@ -123,7 +124,7 @@ get_header(); ?>
 			  </thead>
 			  <tbody>
 			    <tr>
-			      <td>Content Goes Here</td>
+			      <td><?php echo get_post_custom_values('stage_maxpeople')[0];?></td>
 			    </tr>
 			  </tbody>
 			</table>
