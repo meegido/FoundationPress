5ce6b20cbb48959fc1609ecc43bcc4672a0fc2cc
Mercedes Egido <meegido@gmail.com>
2015-08-20 11:57:04 +0200

    dynamic color walker icon with js

--- js/custom/init-foundation.js
+++ js/custom/init-foundation.js
@@ -1,7 +1,16 @@
 jQuery(document).foundation();
 
+
 $( document ).ready(function () {
 	$("#register-button").click(function () {
 		$(".register-form").show();
 	});
+
+	if ($('#register-button')) {
+		var color = $('#register-button').css('background-color');
+		$('#walker-icon').css('color', color);
+	};
+
+
 });
+
