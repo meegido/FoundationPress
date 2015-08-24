c78a1f9848910af6eb5ac2e7dd6c1436a7cd4aa3
Mercedes Egido <meegido@gmail.com>
2015-08-24 12:23:05 +0200

    max with header fixed on laptop

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -8,16 +8,16 @@
  */
 
 ?>
-<div class="top-bar-container contain-to-grid show-for-medium-up">
-    <nav class="top-bar" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <a href = "<?php echo get_bloginfo('wpurl') ?>"><img class="logo" src="<?php echo get_bloginfo('template_url') ?>/assets/img/cicero-logo.png"/></a>
-            </li>
-        </ul>
-        <section class="top-bar-section">
-            <?php foundationpress_top_bar_l(); ?>
-            <?php foundationpress_top_bar_r(); ?>
-        </section>
-    </nav>
+<div class="row">
+    <div class="top-bar-container contain-to-grid show-for-medium-up">
+        <nav class="top-bar" data-topbar role="navigation">
+            <div class="title-area">
+                    <a href = "<?php echo get_bloginfo('wpurl') ?>"><img class="logo" src="<?php echo get_bloginfo('template_url') ?>/assets/img/cicero-logo.png"/></a>
+            </div>
+            <section class="top-bar-section">
+                <?php foundationpress_top_bar_l(); ?>
+                <?php foundationpress_top_bar_r(); ?>
+            </section>
+        </nav>
+    </div>
 </div>
