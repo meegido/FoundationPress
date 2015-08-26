5b891c1e55732a7fb116a06535344bee9af103ae
Mercedes Egido <meegido@gmail.com>
2015-08-26 15:24:04 +0200

    full with template pages margins medium query

--- page.php
+++ page.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
 <div class="row">
-	<div class="small-12 large-12 columns" role="main">
+	<div class="small-12 large-12 columns no-padding" role="main">
 
 	<?php do_action( 'foundationpress_before_content' ); ?>
 
