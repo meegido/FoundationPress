5ebc74f157083d631d62ae8f20ba623c4040cdfe
Mercedes Egido <meegido@gmail.com>
2015-09-03 10:30:26 +0200

    thumbnail header in sidebar-left template

--- templates/page-sidebar-left.php
+++ templates/page-sidebar-left.php
@@ -4,15 +4,28 @@ Template Name: Left Sidebar
 */
 get_header(); ?>
 <div class="row">
+    <header>
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php
+            $thumb_id = get_post_thumbnail_id();
+            $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+            $thumb_url = $thumb_url_array[0];
+            ?>
+
+            <div class="row">
+                <div class="featured-image" style="background-image: url('<?php echo $thumb_url ?>')" >
+                </div>
+            </div>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php endif; ?>
+    </header>
     <div class="small-12 medium-8 large-8 columns no-padding" role="main">
 
         <?php do_action( 'foundationpress_before_content' ); ?>
 
         <?php while ( have_posts() ) : the_post(); ?>
             <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-                <header>
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
+               
                 <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
                 <div class="entry-content">
                     <?php the_content(); ?>
